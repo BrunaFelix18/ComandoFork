2e27014809e3175d72a7a643e5d4c415e54e659c
Bruna Felix <0000983672@senaimgaluno.com.br>
2024-10-24 15:14:56 -0300

    Enviando arquivo

--- ConhendoFork/src/Ex10.java
+++ ConhendoFork/src/Ex10.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
 /*Cálculo de Salário: Leia o salário de 5 funcionários e 
- * informe se cada um deles está abaixo ou acima do salário mínimo.
- */
+ * informe se cada um deles está abaixo ou acima do salário mínimo.*/
 public class Ex10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner (System.in);
+		
+		double salario;
+		
+		for (int i=1; i<= 5; i++){
+		System.out.println("Informe seu salario:");
+		System.out.println("Funcionario "+ i +" :");
+		salario = scanner.nextDouble();
+		
+		if(salario>1412) {
+			System.out.println("Funcionario "+ i + " seu salário está acima do salário mínimo");
+		}
+		else {
+			System.out.println("Funcionario "+ i + " seu salário está abaixo do salário mínimo");
+		}
+		}
 	}
 
 }
